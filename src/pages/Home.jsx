899e5ecc773165ac5a3073e19d7c21ca7f13bea8
JuanAtencio <juan.atenciog@gmail.com>
2024-10-01 17:20:56 -0500

    add proyect

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
+import OurPolicy from "../components/OurPolicy";
+import NewslettersBox from "../components/NewslettersBox";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <LatestCollection />
+      <BestSeller />
+      <OurPolicy />
+      <NewslettersBox />
+    </div>
+  );
+};
+
+export default Home;
